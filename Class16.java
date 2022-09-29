997f1e9d5f312899965eac2965312ce1919401b7
110b13061 <113488308+110b13061@users.noreply.github.com>
2022-09-29 16:52:44 +0800

    Create Class16.java

---
+++ Class16.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class class16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x;
+        int y;
+        int z;
+        int r;
+        double area;
+        double volume;
+        String center;
+
+        x = 3;
+        y = 4;
+        z = 5;
+        r = 1;
+        area = 4*1*2*3.14;
+        volume = (4*3.14*1*1*1)/3;
+        center = (x+","+y+","+z);
+
+        System.out.println(area);
+        System.out.println(volume);
+        System.out.println(center);
+    }
+}
